e4e04f8b7f9458b83df5dc15911a7256846a3132
bibaodi <bibaodi@unimaging.com>
2019-08-17 18:13:29 +0800

    修改为可调用方式, 有打印调试信息

--- imghash3.py
+++ imghash3.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python
-
+## for python3.6
 import glob
 import os
 import sys
@@ -13,29 +13,32 @@ reduce = functools.reduce
 
 def avhash(im):
     if not isinstance(im, Image.Image):
+        print(f"im: {im}")
         im = Image.open(im)
     im = im.resize((8, 8), Image.ANTIALIAS).convert('L')
     avg = reduce(lambda x, y: x + y, im.getdata()) / 64.
     return reduce(lambda x, yz: x | (yz[-1] << yz[0]),enumerate(map(lambda i: 0 if i < avg else 1, im.getdata())),0)
 
 def hamming(h1, h2):
+    print(f"h1:{h1}, h2:{h2}")
     h, d = 0, h1 ^ h2
+    print(f"d: {d}")
     while d:
         h += 1
         d &= d - 1
     return h
 
-if __name__ == '__main__':
-    if len(sys.argv) <= 1 or len(sys.argv) > 3:
-        print(f"Usage: {sys.argv[0]} image.jpg [dir]")
-    else:
-        im, wd = sys.argv[1], '.' if len(sys.argv) < 3 else sys.argv[2]
+def process(img, imgs_dir):
+        print(img, imgs_dir)
+        im = img
+        wd = imgs_dir
         h = avhash(im)
-
+        print(os.getcwd())
         os.chdir(wd)
+        print(os.getcwd())
         images = []
         for ext in EXTS:
-            images.extend(glob.glob('*.%s' % ext))
+            images.extend(glob.glob(f'*.{ext}'))
 
         seq = []
         prog = int(len(images) > 50 and sys.stdout.isatty())
@@ -44,13 +47,21 @@ if __name__ == '__main__':
             if prog:
                 perc = 100. * prog / len(images)
                 x = int(2 * perc / 5)
-                #print '\rCalculating... [' + '#' * x + ' ' * (40 - x) + ']',
                 print(f"\rCalculating... [{'#' * x} {' ' * (40 - x)} ]", end='')
-                #print '%.2f%%' % perc, '(%d/%d)' % (prog, len(images)),
                 print(f"{perc:.2f}% {prog}/{len(images)}", end='')
                 sys.stdout.flush()
                 prog += 1
+            break
 
         if prog: print
         for f, ham in sorted(seq, key=lambda i: i[1]):
             print(f"{ham}\t{f}")
+
+                      
+if 0 and __name__ == '__main__':
+    if len(sys.argv) <= 1 or len(sys.argv) > 3:
+        print(f"Usage: {sys.argv[0]} image.jpg [dir]")
+    else:
+        im, wd = sys.argv[1], '.' if len(sys.argv) < 3 else sys.argv[2]
+        process(im, wd)
+
